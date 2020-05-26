e610eccdb1f0a02d5f630fe422998c63e7e65c42
celrazc <64177086+celrazc@users.noreply.github.com>
2020-05-25 21:39:45 -0500

    app
    tienda
    
    Co-Authored-By: celrazc <celrazc@users.noreply.github.com>

--- src/app/tienda/tienda.component.ts
+++ src/app/tienda/tienda.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent implements OnInit {
+  imagenes = [
+    '../../assets/corte.jpg',
+    '../../assets/spa.jpg',
+    '../../assets/esterilizacion.jpeg',
+    '../../assets/croquetas.jpg',
+    '../../assets/adiestramiento.jpg'
+  ];
 
+  titulos = [
+    'Corte gratis',
+    '20% Descuetno en SPA',
+    'Dia de esterilización',
+    'Nueva marca de croquetas',
+    'Un dia de Adiestramiento'
+  ];
+
+  descripciones = [
+    'Llevate un corte para tu mascota gratis',
+    'Obtén el 20% de descuento para un dia de SPA para tu mascota',
+    'Esteriliza a tu mascota, en este dia, aprovecha la campaña de esterilizacion',
+    'Conoce la nueva marca de croquetas. Una nueva marca que tiene la mejor proteina para tu mascota',
+    'Ven con tu mascota y obten un dia de adiestramiento canino para que mejore sus habilidades de convivencia'
+  ];
   constructor() { }
 
   ngOnInit(): void {
